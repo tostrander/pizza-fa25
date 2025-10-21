ca69c8ca0e236f6382992b1e64b678df334e5fd3
Tina Ostrander <tostrander@greenriver.edu>
2025-10-21 11:19:34 -0700

    Add additional routes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // Enable static file serving
 app.use(express.static('public'));
 
+// Allow the app to parse form data (req.body)
+app.use(express.urlencoded({ extended: true }));
+
+// Create an array to store orders
+const orders = [];
+
 // Define the port number where our server will listen
 const PORT = 3001;
 
@@ -18,6 +24,47 @@ app.get('/', (req, res) => {
     // Send a response to the client
     // res.send(`<h1>Welcome to Poppa\'s Pizza!</h1>`);
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Define a "contact-us" route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// Define a "confirmation" route
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Define an "admin" route
+app.get('/admin', (req, res) => {
+
+    res.send(orders);
+    //res.sendFile(`${import.meta.dirname}/views/admin.html`);
+});
+
+// Define an "submit-order" route
+app.post('/submit-order', (req, res) => {
+
+    //console.log(req.body);
+
+    // Create a JSON object to store the data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size,
+        comment: req.body.comment
+      };
+
+      // Add order to array
+      orders.push(order);
+      console.log(orders);
+
+      // Send user to confirmation page
+      res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
 // Start the server and make it listen on the port 
